ad23474f0382db1ea2bef7688b06c20959285778
Lucas Barrozo <lucaslbarrozo@gmail.com>
2020-06-22 18:31:53 -0300

    Add project files.

---
+++ Gerenciamento_aniversario_ASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gerenciamento_aniversario_ASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
